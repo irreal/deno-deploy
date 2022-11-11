6ab16fb8bacf16f79bcbe70898c079830ac0bad2
Milos Spasojevic <milos.s.pfc@gmail.com>
2022-11-12 00:09:36 +0100

    test deploy

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  const response = new Response("Hello World! brand new", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
